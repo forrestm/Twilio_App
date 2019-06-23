faff9017f87f8c956689a7c2fea9a2f423697720
Forrest <11200528+forrestm@users.noreply.github.com>
2019-06-22 22:42:27 -0500

    Cleaning Up and Working Class
    Hid numbers and commited run. Deleted unused files. Class is currently working

--- Budget.py
+++ Budget.py
@@ -10,9 +10,13 @@ CURRENT_DATETIME = date.today()
 
 # Formats the date for filename
 CURRENT_DATE = CURRENT_DATETIME.strftime("%b-%d-%Y")
+CURRENT_DATE_VAR = CURRENT_DATETIME.strftime("%A-%m-%d")
 
 # Creates the final file with extension string
 CURRENT_DATE_YAML = CURRENT_DATE + ".yaml"
+
+GROCERIES_WK_BUDGET = 100
+OTHER_WK_BUDGET = 250
 
 # Needs to be changed to correct path
 # path = Path.home().joinpath('Documents', 'f-mo', 'Twilio_App', 'Weekly_Charges', f'{CURRENT_DATE_YAML}')
@@ -42,11 +46,6 @@ class Budget(object):
                                     f'{CURRENT_DATE_YAML}')
     def __init__(self, body):
         self.body = body
-        self.chase_list = self.body.split()
-        self.amount = float(self.chase_list[6][1:])
-        self.date = datetime.strptime(self.chase_list[-11], '%m/%d/%Y')
-        self.business = " ".join(self.chase_list[8:-12])
-        self.charge_date = self.date.strftime("%A-%m-%d")
 
     def write_to(self, charges):
         self.charges = charges
@@ -59,15 +58,49 @@ class Budget(object):
         return charges
 
     def Received_Bank_text(self):
+        chase_list = self.body.split()
+        amount = float(chase_list[6][1:])
+        date = datetime.strptime(chase_list[-11], '%m/%d/%Y')
+        business = " ".join(chase_list[8:-12])
+        charge_date = date.strftime("%A-%m-%d")
         charges = self.read_from()
-        charges[self.charge_date].append(self.amount)
+        charges[charge_date].append(amount)
         self.write_to(charges)
+        return f'Lawd, you spent ${amount} at {business}?!'
+
+    def Groceries_Yes(self):
+        charges = self.read_from()
+        grocery_charge = charges[CURRENT_DATE_VAR].pop(-1)
+        charges['Groceries'].append(grocery_charge)
+        current_groceries = GROCERIES_WK_BUDGET - sum(charges['Groceries'])
+        self.write_to(charges)
+        return f'You got ${current_groceries} left for groceries baby'
+
+    def Undo(self):
+        charges = self.read_from()
+        undo_charge = charges[CURRENT_DATE_VAR].pop(-1)
+        self.write_to(charges)
+        return f'Undid ${undo_charge}'
+
+    def Current_Amounts(self, grocery):
+        self.grocery = grocery
+        charges = self.read_from()
+        if grocery:
+            current_groceries = GROCERIES_WK_BUDGET - sum(charges['Groceries'])
+            return f'You got ${current_groceries} left for groceries baby'
+        else:
+            current_budget = OTHER_WK_BUDGET - sum([sum(item) for key,item in charges.items()])
+            return f'You got ${round(current_budget, 2)} left for the week sugar'
+
+    def Help(self):
+        return u'I am your personal helper \U0001F481'
+
     
-    def __str__(self):
-        return f'Spent ${self.amount} at {self.business} on {self.charge_date}'
+    # def __str__(self):
+    #     return f'Spent ${self.amount} at {self.business} on {self.charge_date}'
     
-    def __repr__(self):
-        return f'Spent ${self.amount} at {self.business} on {self.charge_date}'
+    # def __repr__(self):
+    #     return f'Spent ${self.amount} at {self.business} on {self.charge_date}'
 
 
 
---
+++ run.py
@@ -0,0 +1,63 @@
+# /usr/bin/env python
+# Download the twilio-python library from twilio.com/docs/libraries/python
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
+import os
+from twilio.rest import Client
+from Budget import Budget
+
+app = Flask(__name__)
+
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+client = Client(account_sid, auth_token)
+
+fmo = os.environ['NUMBER']
+twilio_number = os.environ['TWILIO_NUMBER']
+
+@app.route("/sms", methods=['GET', 'POST'])
+def incoming_sms():
+    """Send a dynamic reply to an incoming text message"""
+    # Get the message the user sent our Twilio number
+    body = request.values.get('Body', None)
+    bud = Budget(body)
+    # Start our TwiML response
+    resp = MessagingResponse()
+
+    if body in ['Yes', 'YES', 'Yeah', 'yeah', 'y', 'yes']:
+        chase_resp = bud.Groceries_Yes()
+        client.messages.create(body=chase_resp,from_=twilio_number, to=fmo)
+        
+    elif body in ['Undo', 'undo']:
+        chase_resp = bud.Undo()
+        client.messages.create(body=chase_resp,from_=twilio_number, to=fmo)
+        
+    elif body in ['g?', 'G?']:
+        chase_resp = bud.Current_Amounts(True)
+        client.messages.create(body=chase_resp,from_=twilio_number, to=fmo)
+        
+    elif body in ['Help', 'help']:
+        bud.Help()
+        
+    elif " ".join(body.split()[0:2]) == "Chase account":
+        chase_resp = bud.Received_Bank_text()
+        client.messages.create(body=chase_resp,from_=twilio_number, to=fmo)
+
+    else:
+        chase_resp = bud.Current_Amounts(False)
+        client.messages.create(body=chase_resp,from_=twilio_number, to=fmo)
+
+    # return str(resp)
+
+@app.route("/")
+def hello():
+    return "<h1 style='color:blue'>Hello There!</h1>"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
+
+##############################################################
+
+
+
+# Cronjob to run a python script that will append a Yaml entry with the weeks setup
